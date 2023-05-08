155b59fdc362010b66222b83881dd1ed04ab23c4
lin.wang <lin.wang@confluxnetwork.org>
2023-05-08 17:31:57 +0800

    add top menu bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
+import Head from 'next/head';
 import '../css/global.css';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Head>
+                <title>NFTRainbow - One Stop NFT Infrastructure & Service</title>
+                <link rel="shortcut icon" href="/images/logo.svg" type="image/svg+xml"></link>
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,45 @@
+import Image from 'next/image';
+function Menu() {
+    return (
+        <div className="relative h-[74px] min-w-[1200px] px-[120px] flex flex-row justify-between text-base font-normal text-[#05001F]">
+            <div className="flex flex-row items-center">
+                <a href="/" className="flex flex-row items-center text-[#010101]">
+                    <Image width={28} height={28} src="/images/logo.svg" alt="NFTRainbow" />
+                    <div className="ml-1 font-semibold">NFTRainbow</div>
+                </a>
+                <a href="/" className="ml-[28px]">
+                    Exploration activities
+                </a>
+                <a href="/" className="flex flex-row items-center ml-8">
+                    Solution
+                    <Image className="ml-2" width={16} height={16} src="/images/arrow.svg" alt="arrow" />
+                </a>
+                <a href="/" className="ml-8">
+                    Development documentation
+                </a>
+            </div>
+            <div className="flex flex-row items-center">
+                <div className="flex flex-row items-center">
+                    <Image width={16} height={16} src="/images/language.svg" alt="language" />
+                    <div className="ml-1">EN</div>
+                </div>
+                <button className="ml-[18px] py-[10px] px-4 rounded-full text-sm leading-[18px] text-white font-normal bg-[#6953EF] border border-[#37334C]">
+                    Connect Wallet
+                </button>
+            </div>
+            <div
+                style={{
+                    background: 'linear-gradient(269.99deg, #6953EF 0%, #59E213 34.3%, #FEE64E 66.5%, #FF003C 99.99%)',
+                }}
+                className="w-60 h-0.5 absolute top-full left-0"></div>
+        </div>
+    );
+}
+
 export default function Index() {
-    return <div className="text-3xl text-red-500">website</div>;
+    return (
+        <div className="flex flex-col">
+            <Menu />
+        </div>
+    );
 }
