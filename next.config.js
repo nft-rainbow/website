cb65d648c4eb3c8b19013777b8ba29866c96d9b7
lin.wang <lin.wang@confluxnetwork.org>
2023-05-09 14:15:58 +0800

    add AppModule and DigitalIdentityNetwork

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    i18n: {
+        locales: ['en', 'zh-CN'],
+        defaultLocale: 'en',
+    },
     images: {
         unoptimized: true,
     },
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,37 +2,38 @@ import Image from 'next/image';
 import { useState } from 'react';
 function Menu() {
     return (
-        <div className="relative z-[11] mx-auto w-[1200px] h-[74px] flex flex-row justify-between text-base font-normal text-[#05001F]">
+        <div className="mx-auto w-[1200px] h-[74px] flex flex-row justify-between text-base font-normal text-[#05001F]">
             <div className="flex flex-row items-center">
                 <a href="/" className="flex flex-row items-center text-[#010101]">
                     <Image width={28} height={28} src="/images/logo.svg" alt="NFTRainbow" />
                     <div className="ml-1 font-semibold">NFTRainbow</div>
                 </a>
                 <a href="/" className="ml-[28px]">
-                    Exploration activities
+                    探索活动
                 </a>
                 <a href="/" className="flex flex-row items-center ml-8">
-                    Solution
+                    解决方案
                     <Image className="ml-2" width={16} height={16} src="/images/arrow.svg" alt="arrow" />
                 </a>
                 <a href="/" className="ml-8">
-                    Development documentation
+                    开发文档
                 </a>
             </div>
             <div className="flex flex-row items-center">
                 <div className="flex flex-row items-center">
                     <Image width={16} height={16} src="/images/language.svg" alt="language" />
-                    <div className="ml-1">EN</div>
+                    <div className="ml-1">中</div>
                 </div>
-                <button className="ml-[18px] py-[10px] px-4 rounded-full text-sm leading-[18px] text-white font-normal bg-[#6953EF] border border-[#37334C]">
+                {/* <button className="ml-[18px] py-[10px] px-4 rounded-full text-sm leading-[18px] text-white font-normal bg-[#6953EF] border border-[#37334C]">
                     Connect Wallet
-                </button>
+                </button> */}
             </div>
             <div
                 style={{
-                    background: 'linear-gradient(269.99deg, #6953EF 0%, #59E213 34.3%, #FEE64E 66.5%, #FF003C 99.99%)',
+                    backgroundImage: 'linear-gradient(269.99deg, #6953EF 0%, #59E213 34.3%, #FEE64E 66.5%, #FF003C 99.99%)',
+                    backgroundClip: 'border-box',
                 }}
-                className="w-60 h-0.5 absolute top-full left-0"></div>
+                className="absolute left-0 top-[74px] z-[11] w-60 h-0.5"></div>
         </div>
     );
 }
@@ -142,36 +143,65 @@ function Banner() {
 const AppModuleData = [
     {
         title: 'POAP',
+        content:
+            'Rainbow POAP 基于 Conflux 区块链与 NFT Rainbow 数字身份网络，组织和分发轻量级的 NFT 徽章活动，提供快速、便捷、轻量的链上凭证解决方案。基于 Rainbow POAP，开发者或品牌项目方可以个性化定制社区活动，并奖励社区成员 NFT 徽章，目的是社区数量的增长和社区粘性的增强',
+        imageSrc: 'POAP.svg',
     },
     {
         title: 'SBT',
+        content:
+            '基于 Conflux 树图链发行的链上成就 NFT，提供品牌项目发布快速、便捷、轻量的NFT徽章活动。成就 NFT 可以作为品牌项目社区参与的链上记录，为品牌、社区成员轻松、规模化上链的解决方案。',
+        imageSrc: 'SBT.svg',
     },
     {
         title: 'NFT',
+        content: '使社区能够利用数字身份凭证和游戏化的 NFT 活动来更好地奖励和吸引用户。',
+        imageSrc: 'NFT.svg',
     },
     {
         title: '盲盒',
+        content: '使社区能够利用数字身份凭证和游戏化的 NFT 活动来更好地奖励和吸引用户。',
+        imageSrc: 'blindbox.svg',
     },
     {
         title: '白名单\r\n空投',
+        subTitle: '白名单 ｜ 空投',
+        content: '使社区能够利用数字身份凭证和游戏化的 NFT 活动来更好地奖励和吸引用户。',
+        imageSrc: 'whitelist.svg',
     },
     {
         title: '忠诚度策划',
+        content: '使社区能够利用数字身份凭证和游戏化的 NFT 活动来更好地奖励和吸引用户。',
+        imageSrc: 'loyalty.svg',
     },
 ];
 
 function AppModule() {
     const [moduleIndex, setIndex] = useState(0);
+    const { title, imageSrc, subTitle, content } = AppModuleData[moduleIndex];
     return (
-        <div className="mx-auto w-[1200px] pt-18 flex flex-row justify-between">
-            <div className=" text-[28px] font-semibold text-[#473E6B]">应用模块</div>
+        <div className="mx-auto w-[1200px] h-[900px] overflow-visible pt-18 flex flex-row justify-between">
+            <div className="flex flex-col justify-between overflow-visible text-[28px] font-semibold text-[#473E6B]">
+                <span>应用模块</span>
+                <Image className=" -translate-x-14" src="/images/AppModule.svg" alt="AppModuleBg" width={464} height={464} />
+            </div>
             <div className="flex flex-row">
                 <div
                     style={{
                         boxShadow: '8px 12px 0px 0px rgba(235, 234, 255, 1)',
                     }}
-                    className="mr-6 w-[550px] h-[540px] rounded-[32px] border border-[#6B6395]"></div>
-                <div className="flex flex-col">
+                    className="flex flex-col mr-6 p-11 w-[550px] h-[540px] rounded-[32px] border border-[#6B6395]">
+                    <Image src={'/images/' + imageSrc} alt={title} width={130} height={130} />
+                    <div className="flex-1 flex flex-col mt-[42px] text-[#473E6B]">
+                        <div className="flex flex-row items-center">
+                            <div className="text-[30px] leading-[38px] font-semibold ">{subTitle || title}</div>
+                            <Image className="ml-2.5" src={'/images/star.svg'} alt={title} width={28} height={28} />
+                        </div>
+                        <div className="flex-1 mt-[42px] text-base leading-[26px] font-normal">{content}</div>
+                        <Image src="/images/roadmap.svg" alt={title} width={248} height={32} />
+                    </div>
+                </div>
+                <div className="hidden-scroll pb-4 pr-4 -mr-4 flex flex-col overflow-y-scroll">
                     {AppModuleData.map(({ title }, index) => {
                         const isFocus = moduleIndex === index;
                         return (
@@ -183,11 +213,43 @@ function AppModule() {
                                 }}
                                 className={`${
                                     isFocus ? 'bg-[#6953EF] text-[#FAFAFF]' : 'text-[#473E6B]'
-                                } first:mt-0 mt-6 w-[168px] h-[148px] flex items-center justify-center text-xl text-center whitespace-pre-line font-semibold rounded-3xl border border-[#483E6B]`}>
+                                } first:mt-0 mt-6 flex-shrink-0 w-[168px] h-[148px] flex items-center justify-center text-xl text-center whitespace-pre-line font-semibold rounded-3xl border border-[#483E6B]`}>
                                 {title}
                             </div>
                         );
                     })}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+function DigitalIdentityNetwork() {
+    return (
+        <div className="h-[900px] pt-35">
+            <div className="text-[38px] leading-[46px] text-center text-[#37334C]">数字身份网络</div>
+            <div className="mt-12 flex flex-row justify-center">
+                <div
+                    style={{ boxShadow: '8px 12px 0px 0px rgba(235, 234, 255, 1)' }}
+                    className="last:ml-25 pt-12 px-15 flex flex-col items-center w-[550px] h-[382px] rounded-[32px] border border-[#6B6395]">
+                    <Image src="/images/on-chain.svg" width={430} height={130} alt="" />
+                    <div className="mt-9 text-[#473E6B] ">
+                        <div className="text-2xl font-semibold text-center">链上数字身份</div>
+                        <div className="mt-2 text-base font-normal text-center">
+                            通过子图查询或地址快照来查找数据，或从智能合约中查询凭证数据，以定制链上用例
+                        </div>
+                    </div>
+                </div>
+                <div
+                    style={{ boxShadow: '8px 12px 0px 0px rgba(235, 234, 255, 1)' }}
+                    className="last:ml-25 pt-12 px-15 flex flex-col items-center w-[550px] h-[382px] rounded-[32px] border border-[#6B6395]">
+                    <Image src="/images/off-chain.svg" width={430} height={130} alt="" />
+                    <div className="mt-9 text-[#473E6B] ">
+                        <div className="text-2xl font-semibold text-center">链下数字身份</div>
+                        <div className="mt-2 text-base font-normal text-center">
+                            通过集成数据源（例如 Discord、DODO、微信公众号...）贡献数据 通过指定的条件（例如手机号）贡献数据
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -200,13 +262,13 @@ function Partner() {
             <div className="text-[38px] leading-[48px] text-[#473E6B]">合作伙伴</div>
             <div className="mt-12 flex flex-row">
                 <div className="first:ml-0 ml-12 w-60 h-20 flex items-center justify-center rounded-xl border border-[#483E6B]">
-                    <Image src="/images/conflux.svg" alt="conflux" width={148} height={36} />
+                    <Image className="select-none pointer-events-none" src="/images/conflux.svg" alt="conflux" width={148} height={36} />
                 </div>
                 <div className="first:ml-0 ml-12 w-60 h-20 flex items-center justify-center rounded-xl border border-[#483E6B]">
-                    <Image src="/images/anyweb.svg" alt="anyweb" width={144} height={36} />
+                    <Image className="select-none pointer-events-none" src="/images/anyweb.svg" alt="anyweb" width={144} height={36} />
                 </div>
                 <div className="first:ml-0 ml-12 w-60 h-20 flex items-center justify-center rounded-xl border border-[#483E6B]">
-                    <Image src="/images/cellar.svg" alt="cellar" width={111} height={36} />
+                    <Image className="select-none pointer-events-none" src="/images/cellar.svg" alt="cellar" width={111} height={36} />
                 </div>
             </div>
         </div>
@@ -219,6 +281,7 @@ export default function Index() {
             <Menu />
             <Banner />
             <AppModule />
+            <DigitalIdentityNetwork />
             <Partner />
             <Footer />
         </div>
